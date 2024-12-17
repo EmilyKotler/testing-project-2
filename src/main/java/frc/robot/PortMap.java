148f3159f0df8136d94695cb858b095c6a92a889
EmilyKotler <emilykotler@gmail.com>
2024-12-17 15:18:10 +0200

    PortMap

--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -1,4 +1,33 @@
 
 package frc.robot;
 
-public class PortMap {}
+public class PortMap {
+
+    public class controllers {
+        public static final int DRIVER_CONTROLLER = 0;
+    }
+
+    public class Intake {
+        public static final int INTAKE_MOTOR_RIGHT_ID = 5;
+        public static final int INTAKE_MOTOR_LEFT_ID = 6;
+    }
+
+    public class Shooter {
+        public static final int SHOOTER_MOTOR_LEFT_ID = 10;
+        public static final int SHOOTER_MOTOR_RIGHT_ID = 11;
+    }
+
+    public class DriveTrain {
+        public static final int DRIVETRAIN_MOTOR_1_ID = 1;
+        public static final int DRIVETRAIN_MOTOR_2_ID = 2;
+        public static final int DRIVETRAIN_MOTOR_3_ID = 3;
+        public static final int DRIVETRAIN_MOTOR_4_ID = 4;
+    }
+
+    public class Conveyor {
+        public static final int CONVEYOR_MOTOR_ID = 8;
+        public static final int CONVEYOR_IR_1_DIO = 1;
+        public static final int CONVEYOR_IR_2_DIO = 2;
+        public static final int CONVEYOR_IR_3_DIO = 3;
+    }
+}
