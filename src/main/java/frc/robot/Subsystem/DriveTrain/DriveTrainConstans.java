4e29caba65fe4ac4480185ee10a309c2cff60beb
EmilyKotler <emilykotler@gmail.com>
2024-12-17 15:15:26 +0200

    Constans

--- src/main/java/frc/robot/Subsystem/DriveTrain/DriveTrainConstans.java
+++ src/main/java/frc/robot/Subsystem/DriveTrain/DriveTrainConstans.java
@@ -1,3 +1,12 @@
+ 
+package frc.robot.Subsystem.DriveTrain;
+
+ 
+public class DriveTrainConstans {
+
+    public static final double GEAR = 5.65;
+    
+    public static final int DRIVETRAIN_MOTOR_1_VOLT = 7;
     public static final int DRIVETRAIN_MOTOR_2_VOLT = 7;
     public static final int DRIVETRAIN_MOTOR_3_VOLT = -7;
     public static final int DRIVETRAIN_MOTOR_4_VOLT = -7;
